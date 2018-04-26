84cffb62491cc9bdc9ba19aa115f1c4b3f426967
Martin Lobell <martin.lobell@gmail.com>
2018-04-26 13:38:33 +0200

    DB

--- project-files/article_app.py
+++
@@ -1,9 +0,0 @@
-from bottle import route, run, template
-import article_collection
-
-@route('/')
-def index():
-    articles = article_collection.get_articles()
-    return template('index', articles=articles)
-
-run(host='localhost', port=8080)
