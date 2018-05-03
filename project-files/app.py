7e15a983b1410272f958afdfd5e5f44bd0b41771
Martin Lobell <martin.lobell@gmail.com>
2018-05-03 10:18:13 +0200

    Commit changes

--- project-files/app.py
+++ project-files/app.py
@@ -1,15 +1,7 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from flask_mysqldb import MySQL
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> ec6c163a11965df929b9fa828f787e1f7184d7cd
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-import calendar
->>>>>>> ec6c163a11965df929b9fa828f787e1f7184d7cd
-'''import account_collection'''
 
 app = Flask(__name__)
 
@@ -41,42 +33,36 @@ def calendar():
 def cleaning_tips():
     return render_template("cleaning_tips.html", title="Städtips")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 @app.route("/register.html/")
-=======
 
 @app.route("/register.html/", methods=["GET", "POST"])
->>>>>>> ec6c163a11965df929b9fa828f787e1f7184d7cd
-=======
 
 @app.route("/register.html/", methods=["GET", "POST"])
->>>>>>> ec6c163a11965df929b9fa828f787e1f7184d7cd
 def register():
     form = Registrera(request.form)
     if request.method == "POST" and form.validate():
         email = form.email.data
         password = sha256_crypt.encrypt(str(form.password.data))
-        
+
         # Create cursor
         cur = mysql.connection.cursor()
-        
-        # Execute query 
+
+        # Execute query
         cur.execute("INSERT INTO user_password(email, password) VALUES (%s, %s)", (email, password))
-        
+
         # Commit to DB
         mysql.connection.commit()
-        
+
         # Close connection
         cur.close()
-        
+
         # Registration message with categories
         flash('Du är nu registrerad och kan logga in', 'success')
-        
+
         redirect(url_for('index'))
-        
+
         return render_template("register.html", form = form)
-    
+
     return render_template("register.html", form=form, title="Registrera", author="Martin")
 
 
@@ -104,8 +90,8 @@ class Registrera(Form):
     email = StringField("E-post", [validators.Length(min=5, max=50)])
     password = PasswordField("Lösenord", [validators.DataRequired(), validators.EqualTo("confirm", message="Fel lösenord")])
     confirm = PasswordField("Bekräfta lösenord")
-    
-    
+
+
 if __name__ == "__main__":
     app.secret_key='secret123'
     app.run(debug=True)
