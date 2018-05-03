65477060a3ef8372aebbbf31efc05afec67a3a0f
Christopher Lindberg <christopher.lindberg1@gmail.com>
2018-05-03 14:30:42 +0200

    Fixade ett mindre fel med login.html

--- project-files/app.py
+++ project-files/app.py
@@ -31,7 +31,7 @@ def get_titlecontent(a):
 #Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'clean417k(dj'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'cudb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
