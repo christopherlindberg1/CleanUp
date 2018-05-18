87359bcc8fbc082cd5b3283c43a73a3b0340f984
Christopher Lindberg <christopher.lindberg1@gmail.com>
2018-05-18 13:26:30 +0200

    La till kommentarer

--- project-files/static/js/alertFade.js
+++ project-files/static/js/alertFade.js
@@ -1,4 +1,6 @@
+// Author: Christopher
 
+// Gör så att flash-meddelandena fadear efter några sekunder
 $(document).ready(function(){
 	setTimeout(function() {
 		$(".fade-out-five-seconds").fadeOut(1000);
