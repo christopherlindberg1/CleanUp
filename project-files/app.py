a9fba01e93a5c4c1918a533831f64613bfe5e47e
Martin Lobell <martin.lobell@gmail.com>
2018-05-22 13:39:57 +0200

    Flyttat funktioner till ny fil.

--- project-files/app.py
+++ project-files/app.py
@@ -4,46 +4,9 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, BooleanFiel
 from passlib.hash import sha256_crypt
 from os import listdir
 from functools import wraps
+from functions.py import get_headlines(), get_title_content(), is_logged_in()
 
 app = Flask(__name__)
-
-#Author: Martin
-def get_headlines():
-    '''Listar namnen på alla text-filer i mappen cleaning_articles, samt tar bort filändelsen.'''
-    headlines = listdir("static/cleaning_articles")
-    headline_list = []
-
-    for headline in headlines:
-        headline = headline[:-4]
-        headline_list.append(headline)
-    return headline_list
-
-
-#Author: Martin
-def get_title_content(a):
-    '''Skapar utifrån textfilerna en väg, en läs-variabel och en variabel för text-filens innehåll.'''
-    headlines = listdir("static/cleaning_articles")
-    headline_list = []
-    for headline in headlines:
-        if headline == a + ".txt":
-            path = "static/cleaning_articles/" + headline
-            fill = days_file = open(path,'r')
-            text = fill.read()
-        else:
-            print("test")
-    return (a + ": " + text)
-
-
-def is_logged_in(f):
-    ''' Används för att göra vissa sidor synliga för endast inloggade användare '''
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if "logged_in" in session:
-            return f(*args, **kwargs)
-        else:
-            flash("För att ta del av den här sidan måste du logga in", "primary")
-            return redirect(url_for("login"))
-    return wrap
 
 
 #Config MySQL
