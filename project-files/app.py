e38d94234268c44de04a09db1452b4c806c58dff
Christopher Lindberg <christopher.lindberg1@gmail.com>
2018-05-22 22:42:13 +0200

    Tog bort lösen

--- project-files/app.py
+++ project-files/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'clean417k(dj'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'cudb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
