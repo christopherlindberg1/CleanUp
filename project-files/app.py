b8a5abfb19d9ad7dea9fe811722b6f2342e08450
Josef Degavro <josef.degavro@gmail.com>
2018-05-23 13:57:06 +0200

    Updated db-connection to 127.0.0.1

--- project-files/app.py
+++ project-files/app.py
@@ -10,7 +10,7 @@ from forms import Register, Login
 app = Flask(__name__)
 
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'cudb'
