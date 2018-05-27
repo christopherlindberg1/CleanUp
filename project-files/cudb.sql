8d6d77cac6a8aaeb425c3f47c82928571f201f67
Christopher Lindberg <christopher.lindberg1@gmail.com>
2018-05-27 22:52:50 +0200

    Uppdaterade filen så att den stämmer överens med de tabeller gruppen har skapat

--- project-files/cudb.sql
+++ project-files/cudb.sql
@@ -1,238 +1,37 @@
-\set ON_ERROR_STOP ON
-\c ah8210
-drop database if exists cudb;
-create database cudb;
-\c cudb;
+use cudb;
 
-	-- Tabell med två kolumner; användarnamn och lösenord.
+
+-- Denna fil är en representation över de tabeller som finns i vår databas.
+
+
+-- Innehåller information om användare
 CREATE TABLE user_password (
- 	--Ser till att användarnamnet är unikt.
+ 	username VARCHAR(100),
 	email VARCHAR(100) PRIMARY KEY,
  	password VARCHAR(100)
 );
 
-	--Varje kodrad motsvarar en rad i tabellen "user_password". Dvs varje användarnamn skall höra ihop med ett lösenord.
-Insert INTO user_password values
-	('test@gmail.com', 'lösenord1'),
-	('test@hotmail.com', 'lösenord1'),
-	('TESTING@gmail.com', 'lösen')
-;
 
-	-- Tabell med två kolumner; städföremål och rum.
-CREATE TABLE items (
-  name TEXT,
-  room TEXT
+-- Innehåller information om vilka rum varje användare har
+CREATE TABLE room (
+	room_name VARCHAR(50) PRIMARY KEY,
+	email VARCHAR(100) FOREIGN KEY
 );
 
-	--Varje kodrad motsvarar en rad i tabellen "items". Dvs varje städföremål skall höra ihop med ett rum.
-Insert INTO items values
-	-- Kök
-	('ugn', 'kök'),
-	('diskbänk', 'kök'),
-	('spis', 'kök'),
-	('spisfläkt', 'kök'),
-	('bord', 'kök'),
-	('köksluckor', 'kök'),
-	('kylskåp', 'kök'),
-	('frys', 'kök'),
-	('mikrovågsugn', 'kök'),
-	('brödrost', 'kök'),
-	('kaffebryggare', 'kök'),
-	('diskmaskin', 'kök'),
-	('skafferi', 'kök'),
-	-- Sovrum
-	('säng', 'sovrum'),
-	('byrå', 'sovrum'),
-	('garderob', 'sovrum'),
-	('nattduksbord', 'sovrum'),
-	-- Vardagsrum
-	('soffa', 'vardagsrum'),
-	('fotölj', 'vardagsrum'),
-	('tv', 'vardagsrum'),
-	('tv-bänk', 'vardagsrum'),
-	('soffbord', 'vardagsrum'),
-	('matbord', 'vardagsrum'),
-	('bokhylla', 'vardagsrum'),
-	('fotpall', 'vardagsrum'),
-	-- Badrum
-	('badkar', 'badrum'),
-	('toalett', 'badrum'),
-	('handfat', 'badrum'),
-	('vattenlås', 'badrum'),
-	('badrumsmatta', 'badrum'),
-	('badrumsspegel', 'badrum'),
-	('medicinskåp', 'badrum'),
-	('tvättmaskin', 'badrum'),
-	('torktumlare', 'badrum'),
-	('golvbrunn', 'badrum'),
-	-- Arbetsrum
-	('skrivbord', 'arbetsrum'),
-	('bokhylla', 'arbetsrum'),
-	-- Hall
-	('skohylla', 'hall'),
-	('spegel', 'hall'),
-	('hatthylla', 'hall'),
-	('dörrmatta', 'hall'),
-	-- Gemensamma föremål
-	('golv', 'alla'),
-	('vägg', 'alla'),
-	('fönster', 'alla'),
-	('lister', 'alla'),
-	('element', 'alla'),
-	('lampa', 'alla'),
-	('matta', 'alla'),
-	('duk', 'alla'),
-	('växt', 'alla'),
-	('gardin', 'alla'),
-	('persienner', 'alla'),
-	('tavla', 'alla')
-;
 
-	-- Tabell med två kolumner; föremål och föremålets syfte.
-CREATE TABLE cleaning_items (
-	name TEXT,
-	utility TEXT
+-- Innehåller information om vilka möbler som finns i olika användares rum
+CREATE TABLE object (
+	object_name VARCHAR(50),
+	room_name VARCHAR(50) FOREIGN KEY,
+	email VARCHAR(100) FOREIGN KEY
 );
 
-	--Varje kodrad motsvarar en rad i tabellen "cleaning_items". Dvs varje föremål skall höra ihop med ett syfte.
-Insert INTO cleaning_items values
-	-- Städverktyg & städmedel
-	('kvast', 'utility'),
-	('toalettborste', 'utility'),
-	('stålull', 'utility'),
-	('disktrasa', 'utility'),
-	('moppmedhink', 'utility'),
-	('våttork', 'utility'),
-	('dammsugare', 'utility'),
-	('skyffelborste', 'utility'),
-	('dammvippa', 'utility'),
-	('mikrofibertrasa', 'utility'),
-	('tvättsvamp', 'utility'),
-	('skurmedel', 'utility'),
-	('diskmedel', 'utility'),
-	('fönstertvätt', 'utility'),
-	('såpa', 'utility'),
-	('wc-anka', 'utility'),
-	('handsprit', 'utility'),
-	('rengöringsmedel', 'utility')
-;
-=======
-\set ON_ERROR_STOP ON
-\c ah8210
-drop database if exists cudb;
-create database cudb;
-\c cudb;
 
-	-- Tabell med två kolumner; användarnamn och lösenord.
-CREATE TABLE user_password (
- 	--Ser till att användarnamnet är unikt.
-	email VARCHAR(100) PRIMARY KEY,
- 	password VARCHAR(100)
+-- Lagrar artiklar
+CREATE TABLE article (
+	id INT(11) AUTO INCREMENT PRIMARY KEY,
+	title VARCHAR(100),
+	author VARCHAR(100),
+	body TEXT,
+	create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-	--Varje kodrad motsvarar en rad i tabellen "user_password". Dvs varje användarnamn skall höra ihop med ett lösenord.
-Insert INTO user_password values
-	('test@gmail.com', 'lösenord1'),
-	('test@hotmail.com', 'lösenord1'),
-	('TESTING@gmail.com', 'lösen')
-;
-
-	-- Tabell med två kolumner; städföremål och rum.
-CREATE TABLE items (
-  name TEXT,
-  room TEXT
-);
-
-	--Varje kodrad motsvarar en rad i tabellen "items". Dvs varje städföremål skall höra ihop med ett rum.
-Insert INTO items values
-	-- Kök
-	('ugn', 'kök'),
-	('diskbänk', 'kök'),
-	('spis', 'kök'),
-	('spisfläkt', 'kök'),
-	('bord', 'kök'),
-	('köksluckor', 'kök'),
-	('kylskåp', 'kök'),
-	('frys', 'kök'),
-	('mikrovågsugn', 'kök'),
-	('brödrost', 'kök'),
-	('kaffebryggare', 'kök'),
-	('diskmaskin', 'kök'),
-	('skafferi', 'kök'),
-	-- Sovrum
-	('säng', 'sovrum'),
-	('byrå', 'sovrum'),
-	('garderob', 'sovrum'),
-	('nattduksbord', 'sovrum'),
-	-- Vardagsrum
-	('soffa', 'vardagsrum'),
-	('fotölj', 'vardagsrum'),
-	('tv', 'vardagsrum'),
-	('tv-bänk', 'vardagsrum'),
-	('soffbord', 'vardagsrum'),
-	('matbord', 'vardagsrum'),
-	('bokhylla', 'vardagsrum'),
-	('fotpall', 'vardagsrum'),
-	-- Badrum
-	('badkar', 'badrum'),
-	('toalett', 'badrum'),
-	('handfat', 'badrum'),
-	('vattenlås', 'badrum'),
-	('badrumsmatta', 'badrum'),
-	('badrumsspegel', 'badrum'),
-	('medicinskåp', 'badrum'),
-	('tvättmaskin', 'badrum'),
-	('torktumlare', 'badrum'),
-	('golvbrunn', 'badrum'),
-	-- Arbetsrum
-	('skrivbord', 'arbetsrum'),
-	('bokhylla', 'arbetsrum'),
-	-- Hall
-	('skohylla', 'hall'),
-	('spegel', 'hall'),
-	('hatthylla', 'hall'),
-	('dörrmatta', 'hall'),
-	-- Gemensamma föremål
-	('golv', 'alla'),
-	('vägg', 'alla'),
-	('fönster', 'alla'),
-	('lister', 'alla'),
-	('element', 'alla'),
-	('lampa', 'alla'),
-	('matta', 'alla'),
-	('duk', 'alla'),
-	('växt', 'alla'),
-	('gardin', 'alla'),
-	('persienner', 'alla'),
-	('tavla', 'alla')
-;
-
-	-- Tabell med två kolumner; föremål och föremålets syfte.
-CREATE TABLE cleaning_items (
-	name TEXT,
-	utility TEXT
-);
-
-	--Varje kodrad motsvarar en rad i tabellen "cleaning_items". Dvs varje föremål skall höra ihop med ett syfte.
-Insert INTO cleaning_items values
-	-- Städverktyg & städmedel
-	('kvast', 'utility'),
-	('toalettborste', 'utility'),
-	('stålull', 'utility'),
-	('disktrasa', 'utility'),
-	('moppmedhink', 'utility'),
-	('våttork', 'utility'),
-	('dammsugare', 'utility'),
-	('skyffelborste', 'utility'),
-	('dammvippa', 'utility'),
-	('mikrofibertrasa', 'utility'),
-	('tvättsvamp', 'utility'),
-	('skurmedel', 'utility'),
-	('diskmedel', 'utility'),
-	('fönstertvätt', 'utility'),
-	('såpa', 'utility'),
-	('wc-anka', 'utility'),
-	('handsprit', 'utility'),
-	('rengöringsmedel', 'utility')
-;
->>>>>>> ec6c163a11965df929b9fa828f787e1f7184d7cd
